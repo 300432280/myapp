88026eabe80112c69b0555e097ac301297658db5
Ubuntu <ubuntu@ip-172-31-31-237.ec2.internal>
2019-12-28 19:01:26 +0000

    add mongoose

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,3 +2,7 @@ exports.get_landing = function(req, res, next) {
 	console.log('hellow world');
  	res.render('landing', { title: 'Express' });
 }
+exports.submit_lead = function(req, res, next) {
+	console.log("emial variable: " + req.body.lead_email);
+ 	res.redirect('/');
+}
